2b761d148d4b7bbd279b7a018b12ef94ec924762
Jonathan <38092600+herrj1@users.noreply.github.com>
2018-06-02 23:34:36 -0400

    Create modalSubmitForm.php

---
+++ handcode/modalSubmitForm.php
@@ -0,0 +1,15 @@
+<form action="{{ url('contact')}}" method="POST">
+    {{ csrf_field() }}
+    <h5 class="mb-2">Basic Info</h5>
+    <div class="form-group">
+       <label for="name">Name:</label>
+       <input type="name" name="name" class="form-control" id="name">
+    </div>
+    <div class="form-group">
+       <label for="email">Email:</label>
+       <input type="email" name="email" class="form-control" id="email">
+    </div>
+    <div class="form-group">
+       <label name="message">Message:</label>
+       <input id="subject" name="message" class="form-control">
+</div>
