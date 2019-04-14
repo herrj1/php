d45e1fe461d88fcfe1ad27bad73fcc85c43def42
Jonathan <38092600+herrj1@users.noreply.github.com>
2019-04-13 22:43:49 -0400

    Update modalSubmitForm.php

--- handcode/modalSubmitForm.php
+++ handcode/modalSubmitForm.php
@@ -1,12 +1,10 @@
 <form action="{{ url('contact')}}" method="POST">
     {{ csrf_field() }}
     <h5 class="mb-2">Basic Info</h5>
-    
     <div class="form-group">
        <label for="name">Name:</label>
        <input type="name" name="name" class="form-control" id="name">
     </div>
-    
     <div class="form-group">
        <label for="email">Email:</label>
        <input type="email" name="email" class="form-control" id="email">
