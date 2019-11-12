d1e337afc81bf7af76170d7fcd562e89883510ff
Jonathan <38092600+herrj1@users.noreply.github.com>
2019-11-11 23:33:53 -0500

    Update web.php

--- lumen/app3/routes/web.php
+++ lumen/app3/routes/web.php
@@ -7,7 +7,6 @@
 | Here is where you can register all of the routes for an application.
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
-|
 */
 
 $router->get('/', function () use ($router) {
