b6fe88088b442da59e05ef6045727b0e1cf181a9
Jonathan <38092600+herrj1@users.noreply.github.com>
2019-11-17 22:38:55 -0500

    Update modalSubmitForm.php

--- handcode/modalSubmitForm.php
+++ handcode/modalSubmitForm.php
@@ -4,10 +4,8 @@
     <div class="form-group">
        <label for="name">Name:</label>
         <input type="name" name="name" class="form-control" id="name">
-    </div>    <div class="form-group">
-       <label for="email">Email:</label>
+    </div><div class="form-group"><label for="email">Email:</label>
        <input type="email" name="email" class="form-control" id="email">
-    </div> <div class="form-group">
-       <label name="message">Message:</label>
+    </div> <div class="form-group"><label name="message">Message:</label>
        <input id="subject" name="message" class="form-control">
 </div>
