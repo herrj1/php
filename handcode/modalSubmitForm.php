3a6c286ad4d6ff93ec33ff633984e93fe4ea8c60
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-01-20 23:21:18 -0500

    Update modalSubmitForm.php

--- handcode/modalSubmitForm.php
+++ handcode/modalSubmitForm.php
@@ -2,9 +2,9 @@
     {{ csrf_field() }}
     <h5 class="mb-2">Basic Info</h5>
     <div class="form-group"><label for="name">Name:</label>
-        <input type="name" name="name" class="form-control" id="name">
+    <input type="name" name="name" class="form-control" id="name">
     </div><div class="form-group"><label for="email">Email:</label>
-       <input type="email" name="email" class="form-control" id="email">
+    <input type="email" name="email" class="form-control" id="email">
     </div> <div class="form-group"><label name="message">Message:</label>
-       <input id="subject" name="message" class="form-control">
+    <input id="subject" name="message" class="form-control">
 </div>
